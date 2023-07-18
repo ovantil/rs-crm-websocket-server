4eb710541507ebebaee3342e2a83da9766d98304
ovantil <81196500+ovantil@users.noreply.github.com>
2023-07-18 16:18:13 +0100

    ignore

--- main.py
+++ main.py
@@ -39,5 +39,4 @@ def hello_world():
 
 
 if __name__ == '__main__':
-    # If we use port 443, we can use HTTPS.
-    socketio.run(app, host='0.0.0.0', port=443)
+    socketio.run(app, host='0.0.0.0', port=5000)
